5521a0679b0821f2462c4f0c63c70545e35006af
Manoel Matias <manoel13matias@gmail.com>
2020-10-02 22:41:07 -0300

    Criei a Aula05.

---
+++ Programação-Estruturada/src/aula05/atividade01/Atividade04.java
@@ -0,0 +1,14 @@
+package aula05.atividade01;
+
+public class Atividade04
+{
+    public static void main(String[] args)
+    {
+        int cont = 1;
+        for(int i = 1; i < 10; i = i)
+        {
+            System.out.println(cont);
+            cont++;
+        }
+    }
+}
